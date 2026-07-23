d730af4fa221e62f7965361783e0b05ecf48dd7f
amarkore <amarnathkore007@gmail.com>
2026-07-23 11:25:26 +0530

    add model training script

--- array/Check_if_the_Array_is_Sorted.py
+++ array/Check_if_the_Array_is_Sorted.py
@@ -1,10 +1,19 @@
-class solution:
-    def checkarrayissort(self,nums):
-        n=len(nums)
-        for i in range(n-1):
-            if nums[i] > nums[i + 1]:
-                return False
-        return True
-nums=[1,2,3,4,5]
-s=solution()
-print(s.checkarrayissort(nums))
+class Solution:
+    def isSorted(self, nums):
+        #your code goes here
+        count = 0
+        n = len(nums)
+
+        for i in range(n):
+            if nums[i] > nums[(i + 1) % n]:
+                count += 1
+
+        return count <= 1
+# Function call
+obj = Solution()
+
+print(obj.isSorted([3,4,5,1,2]))   # True
+print(obj.isSorted([1,2,3,4,5]))   # True
+print(obj.isSorted([2,1,3,4]))     # False
+print(obj.isSorted([1,1,1]))       # True
+
